f48e400bfce5567aa1c56bee575f5fb09a0dd609
Kizzyyyyyy <TomTomZzz@web.de>
2026-07-13 16:36:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -135,4 +135,4 @@ app.post('/api/messages/send', (req, res) => {
     res.json({ success: true });
 });
 
-app.listen(3000, () => console.log('Server läuft auf http://localhost:3000'));
+app.listen(3000, () => console.log('Server läuft auf https://crypto-box.onrender.com'));
